9707efe5bd078daf6c0f5bd829045f41a918b7eb
Kordian Galinski <31996030+kordiangalinski@users.noreply.github.com>
2022-12-04 10:27:06 +0100

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,125 +1,71 @@
 import Head from 'next/head'
 import Link from "next/link";
-import Image from 'next/image';
+import Image from "next/image";
 
-import photo from '../public/mock.png';
-import {useState} from "react";
-
-interface CardProps {
-    title: string
-    content: string
-    date: Date
-    author: string
-}
-
-const MockCardData: CardProps = {
-    title: 'Lorem ipsum et dolor',
-    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ut neque sapien. Integer laoreet lacus elit, non luctus neque luctus in. Suspendisse eu arcu nec velit lacinia suscipit. Nunc pharetra efficitur interdum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eu mattis nibh, a suscipit urna. Pellentesque ultrices cursus libero nec suscipit. Duis et venenatis libero.',
-    date: new Date(),
-    author: 'Kordian Galinski',
-};
-
-const Card = (props: CardProps): JSX.Element => {
+const Home = (): JSX.Element => {
     return (
-        <section className="p-8 shadow-2xl cursor-pointer">
-            <div className="py-2.5">
-                <h1 className="text-2xl text-gray-100">{props.title}</h1>
-                <div className="flex justify-between">
-                    <h2 className="text-gray-400 text-">Created by {props.author}</h2>
-                    <h2 className="text-gray-400">{props.date.toLocaleDateString()}</h2>
-                </div>
+        <div className="bg-slate-800 w-screen min-h-screen flex">
+            <Head>
+                <title>~/usr/KordianGaliński | Home</title>
+            </Head>
+            <div className="mx-auto container flex justify-center items-center md:flex-row flex-col">
+                <Navigation />
+                <MainWrapper />
             </div>
-            <Image src={photo} alt='lex' height={300} className="my-2"/>
-            <p className="text-lg text-gray-300 text-justify">{props.content}</p>
-        </section>
-    )
-}
-
-const About = (): JSX.Element => {
-    return (
-        <div className="flex flex-col justify-center items-center p-6 shadow-2xl">
-            <Image src={photo} alt='lex' height={300}/>
-            <h1 className="text-2xl p-6 text-gray-100">Kordian Galinski</h1>
-            <p className="text-lg text-justify text-gray-200">{MockCardData.content}</p>
+            <Footer/>
         </div>
-    )
-}
-
-const MainContainer = (): JSX.Element => {
-    return (
-        <main className="flex flex-wrap flex-1 justify-center flex-col gap-4 items-center p-6">
-            <div>
-                <h1 className="text-3xl p-6 text-gray-100">Who am I?</h1>
-                <About/>
-            </div>
-            <div>
-                <h1 className="text-3xl p-6 text-gray-100">Latest Activity</h1>
-                <Card title={MockCardData.title} content={MockCardData.content} date={MockCardData.date} author={MockCardData.author}/>
-                <Card title={MockCardData.title} content={MockCardData.content} date={MockCardData.date} author={MockCardData.author}/>
-                <Card title={MockCardData.title} content={MockCardData.content} date={MockCardData.date} author={MockCardData.author}/>
-                <Card title={MockCardData.title} content={MockCardData.content} date={MockCardData.date} author={MockCardData.author}/>
-                <Card title={MockCardData.title} content={MockCardData.content} date={MockCardData.date} author={MockCardData.author}/>
-            </div>
-            <cite className="text-center text-3xl p-6 text-gray-100 w-fit select-none my-6">Evviva l&apos;arte!</cite>
-        </main>
-    )
-}
-
-const FooterNavigation: string[][] = [
-    ['Home', '/'],
-    ['Blog', '/blog'],
-    ['Research', '/research'],
-    ['Projects', '/projects'],
-    ['Instagram', 'https://www.instagram.com/kordiangalinski'],
-    ['GitHub', 'https://www.github.com/kordiangalinski'],
-];
-
-const Footer = (): JSX.Element => {
-    return (
-        <footer className="flex flex-col justify-center p-8 text-slate-50">
-            <nav className="flex gap-x-10 justify-center items-center py-6 flex-wrap">
-                {FooterNavigation.map(
-                    ([title, url], index) => (
-                        <Link key={index} href={url} className="text-lg hover:underline hover:underline-offset-8">{title}</Link>
-                    ))}
-            </nav>
-            <span className="text-center">&copy; Kordian Galiński | 2016 - {new Date().getFullYear()}</span>
-        </footer>
     );
 }
 
-const TopNavigation: string[][] = [
-    ['Home', '/'],
-    ['Blog', '/blog'],
-    ['Research', '/research'],
-    ['Projects', '/projects'],
-    ['Instagram', 'https://www.instagram.com/kordiangalinski'],
-    ['GitHub', 'https://www.github.com/kordiangalinski'],
-];
-
-const TopNavigationBar = (): JSX.Element => {
+const MainWrapper = (): JSX.Element => {
     return (
-        <nav className="w-full flex justify-evenly p-3 text-gray-400 shadow-2xl">
-            {TopNavigation.map(
-                ([title, url], index) => (
-                    <Link key={index} href={url} className="text-lg hover:underline hover:underline-offset-8">{title}</Link>
-                ))}
+        <header className="uppercase text-center p-20 text-slate-200 z-10">
+            <h1 className="text-3xl font-bold">Kordian Galiński</h1>
+            <h2 className="text-xl"> CEO AT @. | Security Engineer | Software Engineer | CTF Team Captain/Player </h2>
+            <main className="flex flex-col items-center justify-center my-6 scroll-auto">
+               <Image
+                   src={"https://i2-prod.mirror.co.uk/incoming/article21174982.ece/ALTERNATES/s1200b/1_Karl-Lagerfeld.jpg"}
+                   alt={'Karl Lagerfeld mock photo'}
+                   width={450}
+                   height={800}
+               />
+            </main>
+        </header>
+    );
+}
+
+const NavigationItems = [
+    {label: "Home", target: "/"},
+    {label: "Who am I?", target: "/about"},
+    {label: "Projects", target: "/projects"},
+    {label: "Research", target: "/research"},
+    {label: "Curriculum Vitae", target: "/cv"},
+    {label: "Blog", target: "/blog"},
+    {label: "Instagram", target: "https://www.instagram.com/kordiangalinski/"},
+]
+
+const Navigation = (): JSX.Element => {
+    return (
+        <nav className="md:border-r md:p-20 text-center border-slate-400">
+            <ul>
+                {NavigationItems && NavigationItems.map((items, index) =>
+                    <Link key={index} href={items.target}>
+                        <li className="my-3 uppercase text-lg font-medium text-slate-300 hover:text-slate-100">
+                            {items.label}
+                        </li>
+                    </Link>
+                )}
+            </ul>
         </nav>
     );
 }
 
-export default function Home(): JSX.Element {
-
+const Footer = () => {
     return (
-        <div className="min-h-screen bg-gray-800">
-            <Head>
-                <title>./kordiangalinski | Home</title>
-            </Head>
-            <div className="container mx-auto ">
-                <TopNavigationBar/>
-                <MainContainer/>
-                <Footer/>
-            </div>
-        </div>
-    )
+        <footer className="fixed bottom-2 text-center w-screen p-4 text-slate-400 font-medium select-none z-0">
+            <span>&#169; Copyright | Kordian Galiński | 2016 - {new Date().getFullYear()}</span>
+        </footer>
+    );
 }
+
+export default Home;
