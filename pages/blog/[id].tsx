afd713b5d29ae30b83c63b92e665c1161e0d0215
Kordian Galinski <31996030+kordiangalinski@users.noreply.github.com>
2022-12-12 23:02:20 +0100

    Create [id].tsx

---
+++ pages/blog/[id].tsx
@@ -0,0 +1,59 @@
+import { useRouter } from 'next/router'
+import Layout from "../../components/Layout";
+import Head from "next/head";
+import Title_Path from "../../utils/title";
+import {useEffect, useState} from "react";
+import {collection, doc, getDoc, getDocs} from "@firebase/firestore";
+import {db} from "../../utils/firebase";
+import Link from "next/link";
+
+const Post = (): JSX.Element => {
+    const router = useRouter()
+    const { id } = router.query
+
+    const [Title, setTitle] = useState(Title_Path);
+    const [Data, setData] = useState([]);
+
+    const fetchPost = async () => {
+
+        // @ts-ignore
+        const docRef = doc(db, 'blog', id);
+        try {
+            const docSnap = await getDoc(docRef);
+            if(docSnap.exists()) {
+                // @ts-ignore
+                setData(docSnap.data());
+                setTitle(Title_Path + Data.title);
+            }
+        } catch(error) {
+            console.log(error)
+        }
+    }
+
+    useEffect(()=>{
+        if(id) {
+            fetchPost();
+        }
+    }, [id])
+
+    return (
+        <>
+            <Head>
+                <title>{Title}</title>
+            </Head>
+            <Layout>
+                {
+                    Data &&
+                    <div className="flex flex-col gap-3.5">
+                        <h1 className="text-4xl text-slate-300">{Data.title}</h1>
+                        <hr className="border-slate-400 border-b-2"/>
+                        <p className="text-slate-300 text-justify">{Data.content}</p>
+                        <Link href={"/blog"} className="text-4xl ml-20 text-slate-300">&#8592;</Link>
+                    </div>
+                }
+            </Layout>
+        </>
+        );
+}
+
+export default Post
